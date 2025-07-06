67c3b7813886b29d6b408e18fba6336175a158db
Karan2624 <karangupta@gmail.com>
2025-07-06 14:00:41 +0530

    added footer

---
+++ src/app/components/Footer.js
@@ -0,0 +1,50 @@
+import styles from "@/app/styles/footer.module.css";
+import Link from "next/link";
+import Image from "next/image";
+
+const Footer = () => {
+    return (
+        <footer className={styles.footer}>
+            <div className={styles.container}>
+                <div className={styles.info}>
+                    <div className={styles.box1}>
+                        <Link href="/">
+                    <Image src="/logo.png" alt="my logo image" width={150} height={50}/>
+                </Link>
+                <p style={{ color: "rgb(223, 210, 210)" }}>Revolutionary 3D fitting technology for mordern the shopper</p>
+                    </div>
+                    <div className={styles.box2}>
+                        <h3 style={{ fontSize: "2rem" , margin: "2rem"}} >Products</h3>
+                
+                            <li><a href="#">Clothing</a></li>
+                            <li><a href="#">Accesories</a></li>
+                            <li><a href="#">Shoes</a></li>
+                           
+
+                    </div>
+                        
+                    <div className={styles.box3}>
+                        <h3 style={{ fontSize: "2rem" , margin: "2rem"}} >Support</h3>
+                
+                            <li><a href="#">Help Center</a></li>
+                            <li><a href="#">Size Guide</a></li>
+                            <li><a href="#">Return</a></li>
+                    </div>
+                    
+                    <div className={styles.box4}>
+                        <h3 style={{ fontSize: "2rem" , margin: "2rem"}} >Company</h3>
+                
+                            <li><a href="#">About Us</a></li>
+                            <li><a href="#">Careers</a></li>
+                            <li><a href="#">Contacts</a></li>
+                    </div>
+                </div>
+                <div className={styles.last}>
+                      <hr className={styles.divider} />
+                       <p className={styles.copy}>© 2025 Fitzo. All rights reserved.</p>
+                </div>
+            </div>
+        </footer>
+    )
+}
+export default Footer;
--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,3 +1,5 @@
+'use client';
+
 import styles from "@/app/styles/navbar.module.css"
 import Link from "next/link";
 import Image from "next/image";
